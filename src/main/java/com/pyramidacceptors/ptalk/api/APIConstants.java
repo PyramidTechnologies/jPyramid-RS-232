43f7daad9be016252155d8efafa7b1b7b34957ef
Cory Todd <corneliustodd@gmail.com>
2015-05-17 20:06:40 -0700

    remove unused constants that are not relevant to API configure cobertura + coveralls + maven to report code coverage

--- src/main/java/com/pyramidacceptors/ptalk/api/APIConstants.java
+++ src/main/java/com/pyramidacceptors/ptalk/api/APIConstants.java
@@ -29,69 +29,10 @@ public abstract class APIConstants {
     /**
      * String response for client code
      */
-    private static final String API_REVISION = "1.1.1.1";
+    private static final String API_REVISION = "1.2.0";
     
     /**
      * Global, default timeout unless otherwise specified
      */
     public static final int COMM_TIMEOUT = 400;
-    
-    /**
-     * These are all forwarded from the jSSC library
-     */
-    public static final int BAUDRATE_110 = 110;
-    public static final int BAUDRATE_300 = 300;
-    public static final int BAUDRATE_600 = 600;
-    public static final int BAUDRATE_1200 = 1200;
-    public static final int BAUDRATE_4800 = 4800;
-    public static final int BAUDRATE_9600 = 9600;
-    public static final int BAUDRATE_14400 = 14400;
-    public static final int BAUDRATE_19200 = 19200;
-    public static final int BAUDRATE_38400 = 38400;
-    public static final int BAUDRATE_57600 = 57600;
-    public static final int BAUDRATE_115200 = 115200;
-    public static final int BAUDRATE_128000 = 128000;
-    public static final int BAUDRATE_256000 = 256000;
-
-    public static final int DATABITS_5 = 5;
-    public static final int DATABITS_6 = 6;
-    public static final int DATABITS_7 = 7;
-    public static final int DATABITS_8 = 8;
-    
-    public static final int STOPBITS_1 = 1;
-    public static final int STOPBITS_2 = 2;
-    public static final int STOPBITS_1_5 = 3;
-    
-    public static final int PARITY_NONE = 0;
-    public static final int PARITY_ODD = 1;
-    public static final int PARITY_EVEN = 2;
-    public static final int PARITY_MARK = 3;
-    public static final int PARITY_SPACE = 4;     
-
-    public static final int PURGE_RXABORT = 0x0002;
-    public static final int PURGE_RXCLEAR = 0x0008;
-    public static final int PURGE_TXABORT = 0x0001;
-    public static final int PURGE_TXCLEAR = 0x0004;
-
-    public static final int MASK_RXCHAR = 1;
-    public static final int MASK_RXFLAG = 2;
-    public static final int MASK_TXEMPTY = 4;
-    public static final int MASK_CTS = 8;
-    public static final int MASK_DSR = 16;
-    public static final int MASK_RLSD = 32;
-    public static final int MASK_BREAK = 64;
-    public static final int MASK_ERR = 128;
-    public static final int MASK_RING = 256;
-
-    public static final int FLOWCONTROL_NONE = 0;
-    public static final int FLOWCONTROL_RTSCTS_IN = 1;
-    public static final int FLOWCONTROL_RTSCTS_OUT = 2;
-    public static final int FLOWCONTROL_XONXOFF_IN = 4;
-    public static final int FLOWCONTROL_XONXOFF_OUT = 8;
-    
-    public static final int ERROR_FRAME = 0x0008;
-    public static final int ERROR_OVERRUN = 0x0002;
-    public static final int ERROR_PARITY = 0x0004;
-
-
 }
